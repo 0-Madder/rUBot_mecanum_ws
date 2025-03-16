cda385277c024ca36b79841f3f31ccca5d910a6a
0-Madder <pablomartinezuni@gmail.com>
2025-03-16 16:43:42 +0000

    Wall follower holonomic done

--- src/rubot_control/src/rubot_wall_follower_holonomic.py
+++ src/rubot_control/src/rubot_wall_follower_holonomic.py
@@ -2,10 +2,12 @@
 import rospy
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import Twist
+import time
 
 pub = None
 d = 0
 vx = 0
+vy = 0
 wz = 0
 vf = 0
 
@@ -14,23 +16,24 @@ scanRangesLengthCorrectionFactor = 2
 
 
 def clbk_laser(msg):
+    # En la primera ejecucion, calculamos el factor de correcion
     global isScanRangesLengthCorrectionFactorCalculated
     global scanRangesLengthCorrectionFactor
     
     if not isScanRangesLengthCorrectionFactorCalculated:
-        scanRangesLengthCorrectionFactor = len(msg.ranges) / 360
-        isScanRangesLengthCorrectionFactorCalculated = True
+            scanRangesLengthCorrectionFactor = len(msg.ranges) / 360
+            isScanRangesLengthCorrectionFactorCalculated = True
 
     front_min= int(0 * scanRangesLengthCorrectionFactor)
     front_max = int(30 * scanRangesLengthCorrectionFactor)
-    fright_min = int(30 * scanRangesLengthCorrectionFactor)
-    fright_max = int(60 * scanRangesLengthCorrectionFactor)
-    right_min = int(60 * scanRangesLengthCorrectionFactor)
-    right_max = int(120 * scanRangesLengthCorrectionFactor)
-    bright_min = int(120 * scanRangesLengthCorrectionFactor)
-    bright_max = int(170 * scanRangesLengthCorrectionFactor)
-    back_min= int(170 * scanRangesLengthCorrectionFactor)
-    back_max = int(190 * scanRangesLengthCorrectionFactor)
+    fright_min = int(270 * scanRangesLengthCorrectionFactor)
+    fright_max = int(330 * scanRangesLengthCorrectionFactor)
+    right_min = int(240 * scanRangesLengthCorrectionFactor)
+    right_max = int(300 * scanRangesLengthCorrectionFactor)
+    bright_min = int(210 * scanRangesLengthCorrectionFactor)
+    bright_max = int(240 * scanRangesLengthCorrectionFactor)
+    back_min= int(150 * scanRangesLengthCorrectionFactor)
+    back_max = int(210 * scanRangesLengthCorrectionFactor)
 
     regions = {
         'bright':  min(min(msg.ranges[bright_min:bright_max]), 3),
@@ -45,52 +48,48 @@ def clbk_laser(msg):
 
 def take_action(regions):
     msg = Twist()
-    linear_x = vx
+    linear_x = 0
     linear_y = 0
-    angular_z = 0  # Eliminamos la rotación
 
     state_description = ''
 
-    # Caso 1: La pared más cercana está a la derecha -> Seguir recto
-    if regions['right'] < regions['front'] and regions['right'] < regions['fright'] and regions['right'] < regions['bright']:
-        state_description = 'case 1 - Wall on the right -> Moving forward'
+    if regions['front'] > d and regions['fright'] > 2*d and regions['right'] > 2*d and regions['bright'] > 2*d:
+        state_description = 'case 1 - nothing'
         linear_x = vx
-        linear_y = 0
-
-    # Caso 2: La pared más cercana está enfrente -> Moverse hacia la izquierda
-    elif regions['front'] < regions['right'] and regions['front'] < regions['fright'] and regions['front'] < regions['bright']:
-        state_description = 'case 2 - Wall in front -> Moving left'
-        linear_x = 0
-        linear_y = vx
-
-    # Caso 3: La pared más cercana está atrás a la derecha -> Moverse en diagonal hacia afuera
-    elif regions['bright'] < regions['front'] and regions['bright'] < regions['fright'] and regions['bright'] < regions['right']:
-        state_description = 'case 3 - Wall behind right -> Moving diagonally outwards'
-        linear_x = vx/3
-        linear_y = vx/6
-
-    # Caso 4: La pared más cercana está adelante a la derecha -> Moverse en diagonal hacia afuera
-    elif regions['fright'] < regions['front'] and regions['fright'] < regions['right'] and regions['fright'] < regions['bright']:
-        state_description = 'case 4 - Wall in front-right -> Moving diagonally outwards'
-        linear_x = vx/6
-        linear_y = -vx/3
+        linear_y = -vy
+    elif regions['front'] < d:
+        state_description = 'case 2 - front'
+        linear_y = vy
+    elif regions['fright'] < d:
+        state_description = 'case 3 - fright'
+        linear_x = vx
+        linear_y = vy
+    elif regions['front'] > d and regions['right'] < d:
+        state_description = 'case 4 - right'
+        linear_x = vx
+    elif regions['bright'] < d:
+        state_description = 'case 5 - bright'
+        linear_x = vx
+        linear_y = -vy
+    elif regions['back'] < d:
+        state_description = 'case 7 - back'
+        linear_x = vx
+    else:
+        state_description = 'case 6 - Far'
+        linear_x = vx
 
     rospy.loginfo(state_description)
     msg.linear.x = linear_x
     msg.linear.y = linear_y
-    msg.angular.z = angular_z
     pub.publish(msg)
     rate.sleep()
-
 
 def shutdown():
     msg = Twist()
     msg.linear.x = 0
     msg.linear.y = 0
-    msg.angular.z = 0
     pub.publish(msg)
     rospy.loginfo("Stop rUBot")
-
 
 def main():
     global pub
@@ -98,24 +97,28 @@ def main():
     global rate
     global d
     global vx
+    global vy
     global wz
     global vf
 
-    rospy.init_node('wall_follower_holonomic')
+    rospy.init_node('wall_follower')
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     sub = rospy.Subscriber('/scan', LaserScan, clbk_laser)
     rospy.on_shutdown(shutdown)
     rate = rospy.Rate(25)
 
-    d = rospy.get_param("~distance_laser")
-    vx = rospy.get_param("~forward_speed")
-    wz = rospy.get_param("~rotation_speed")  # No se usa en holonómico
-    vf = rospy.get_param("~speed_factor")
-
-
+    d= rospy.get_param("~distance_laser")
+    vx= rospy.get_param("~forward_speed")
+    vy= rospy.get_param("~lateral_speed")
+    wz= rospy.get_param("~rotation_speed")
+    vf= rospy.get_param("~speed_factor")
+    
+    
 if __name__ == '__main__':
     try:
         main()
         rospy.spin()
     except rospy.ROSInterruptException:
         shutdown()
+
+
