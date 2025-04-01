1ebfe8d2221f6e4ec81a7305f7d411a97a4becc8
0-Madder <pablomartinezuni@gmail.com>
2025-04-01 17:03:37 +0000

    cambios de clase

--- src/rubot_control/src/rubot_wall_follower_holonomic.py
+++ src/rubot_control/src/rubot_wall_follower_holonomic.py
@@ -23,6 +23,8 @@ class WallFollower:
         self.bleft_distance = 999
         self.left_distance = 999
         self.fleft_distance = 999
+        self.front_distance = 999
+        self.ranges = []
 
         # Velocity publisher
         self.cmd_vel_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
@@ -33,33 +35,18 @@ class WallFollower:
         self.rate = rospy.Rate(10)  # 10 Hz
 
     def laser_callback(self, scan):
-        ranges = np.array(scan.ranges)
-        ranges[np.isinf(ranges)] = 999  # Replace inf values with a large number
-
-        sector = len(ranges)//8
-
-        # Divide the ranges into left and right
-        fright_ranges = ranges[(3 * sector):(4 * sector)]
-        right_ranges = ranges[sector:(3 * sector)]
-        bright_ranges = ranges[0:sector]
-        bleft_ranges = ranges[(7 * sector):]
-        left_ranges = ranges[(5 * sector):(7 * sector)]
-        fleft_ranges = ranges[(4 * sector):(5 * sector)]
-
-        self.fright_distance = np.min(fright_ranges)
-        self.right_distance = np.min(right_ranges)
-        self.bright_distance = np.min(bright_ranges)
-        self.bleft_distance = np.min(bleft_ranges)
-        self.left_distance = np.min(left_ranges)
-        self.fleft_distance = np.min(fleft_ranges)
-
+        self.ranges = np.array(scan.ranges)
         
         
 
 
     def compute_min_distance(self):
         # Determine which side has the closest object
-        if self.fright_distance < self.right_distance and self.fright_distance < self.bright_distance and self.fright_distance < self.bleft_distance and self.fright_distance < self.left_distance and self.fright_distance < self.fleft_distance:
+        if self.front_distance < self.fright_distance and self.front_distance < self.right_distance and self.front_distance < self.bright_distance and self.front_distance < self.bleft_distance and self.front_distance < self.left_distance and self.front_distance < self.fleft_distance:
+            self.closest_distance = self.front_distance
+            self.closest_side = 'front'
+
+        elif self.fright_distance < self.right_distance and self.fright_distance < self.bright_distance and self.fright_distance < self.bleft_distance and self.fright_distance < self.left_distance and self.fright_distance < self.fleft_distance:
             self.closest_distance = self.fright_distance
             self.closest_side = 'fright'
 
@@ -82,42 +69,56 @@ class WallFollower:
             self.closest_distance = self.fleft_distance
             self.closest_side = 'fleft'
 
+    def calcul_ranges(self):
+        self.ranges[np.isinf(self.ranges)] = 999  # Replace inf values with a large number
+
+        sector = len(self.ranges)//8
+
+        # Divide the ranges into left and right
+        fright_ranges = self.ranges[int(2.5 * sector):int(3.5 * sector)]
+        right_ranges = self.ranges[sector:int(2.5 * sector)]
+        bright_ranges = self.ranges[0:sector]
+        bleft_ranges = self.ranges[(7 * sector):]
+        left_ranges = self.ranges[int(5.5 * sector):int(7 * sector)]
+        fleft_ranges = self.ranges[int(4.5 * sector): int(5.5 * sector)]
+        front_ranges = self.ranges[int(3.5 * sector): int(4.5 * sector)]
+
+        self.fright_distance = np.min(fright_ranges)
+        self.right_distance = np.min(right_ranges)
+        self.bright_distance = np.min(bright_ranges)
+        self.bleft_distance = np.min(bleft_ranges)
+        self.left_distance = np.min(left_ranges)
+        self.fleft_distance = np.min(fleft_ranges)
+        self.front_distance = np.min(front_ranges)
 
 
     def run(self):
         msg = Twist()
         while not rospy.is_shutdown():
+            self.calcul_ranges()
             self.compute_min_distance()
             if self.closest_distance < self.safe_distance:
-
-                if self.closest_side == 'fright':
-                    while self.closest_distance < self.max_distance:
-                        msg.linear.x = self.backward_speed
-                    msg.linear.y = self.lateral_speed
-
-                elif self.closest_side == 'right':
-                    while self.closest_distance < self.max_distance:
-                        msg.linear.y = self.lateral_speed    
+                if self.closest_side == 'right':
                     msg.linear.x = self.forward_speed
-
-                elif self.closest_side == 'bright':
-                    while self.closest_distance < self.max_distance:
-                        msg.linear.x = self.forward_speed
-                    msg.linear.y = -(self.lateral_speed)
-
-                elif self.closest_side == 'bleft':
-                    while self.closest_distance < self.max_distance:
-                        msg.linear.x = self.forward_speed
-                    msg.linear.y = -(self.lateral_speed)
-
-                elif self.closest_side == 'left':
-                    while self.closest_side != 'right':
-                        msg.angular.z = self.rotation_speed
-
-                elif self.closest_side == 'fleft':
-                    while self.closest_distance < self.max_distance:
-                        msg.linear.x = self.backward_speed
+                    msg.linear.y = 0
+                elif self.closest_side == 'front':
+                    msg.linear.x = 0
                     msg.linear.y = self.lateral_speed
+                elif self.closest_side == 'fright':
+                    msg.linear.x = self.forward_speed
+                    msg.linear.y = self.lateral_speed
+                elif self.closest_side == 'bright':
+                    msg.linear.x = self.backward_speed
+                    msg.linear.y = -(self.lateral_speed)
+                elif self.closest_side == 'bleft':
+                    pass
+                    #msg.linear.x = self.backward_speed
+                elif self.closest_side == 'left':
+                    pass
+                    #msg.linear.x = self.backward_speed
+                elif self.closest_side == 'fleft':
+                    pass
+                    #msg.linear.x = self.backward_speed
             else:
                 msg.linear.x = self.forward_speed
                 msg.linear.y = 0
